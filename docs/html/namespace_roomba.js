573452661c442b53918f96b42459c05d9c5ff32e
Imtiaz Rahman <63388102+vinzmoke-zoro@users.noreply.github.com>
2024-05-02 22:50:17 -0400

    Delete docs/html directory

--- docs/html/namespace_roomba.js
+++
@@ -1,8 +0,0 @@
-var namespace_roomba =
-[
-    [ "create3_simple_zigzag", "namespace_roomba_1_1create3__simple__zigzag.html", [
-      [ "create_pose_stamped", "namespace_roomba_1_1create3__simple__zigzag.html#a0ba8e60aa87fb718e764b928a865fc00", null ],
-      [ "generate_random_waypoint", "namespace_roomba_1_1create3__simple__zigzag.html#ad3a23af4967dd19b09f16d548871b8df", null ],
-      [ "main", "namespace_roomba_1_1create3__simple__zigzag.html#ad5acbca60aeb983448002452915f1007", null ]
-    ] ]
-];
--- docs/html/search/namespaces_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['roomba_0',['Roomba',['../namespace_roomba.html',1,'']]],
-  ['roomba_3a_3acreate3_5fsimple_5fzigzag_1',['create3_simple_zigzag',['../namespace_roomba_1_1create3__simple__zigzag.html',1,'Roomba']]]
-];
