639f6579b35cf6d32753674fc53f9620173f3ca4
Imtiaz Rahman <63388102+vinzmoke-zoro@users.noreply.github.com>
2024-05-04 03:32:11 -0400

    Delete docs directory

--- docs/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['generate_5frandom_5fwaypoint_0',['generate_random_waypoint',['../namespace_roomba_1_1create3__simple__zigzag.html#ad3a23af4967dd19b09f16d548871b8df',1,'Roomba::create3_simple_zigzag']]]
-];
