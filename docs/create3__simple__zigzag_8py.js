804518f3d14415df6006bd2f477143cc9de62357
Imtiaz Rahman <63388102+vinzmoke-zoro@users.noreply.github.com>
2024-05-03 22:21:33 -0400

    Delete docs directory

--- docs/create3__simple__zigzag_8py.js
+++
@@ -1,6 +0,0 @@
-var create3__simple__zigzag_8py =
-[
-    [ "create_pose_stamped", "create3__simple__zigzag_8py.html#a0ba8e60aa87fb718e764b928a865fc00", null ],
-    [ "generate_random_waypoint", "create3__simple__zigzag_8py.html#ad3a23af4967dd19b09f16d548871b8df", null ],
-    [ "main", "create3__simple__zigzag_8py.html#ad5acbca60aeb983448002452915f1007", null ]
-];
