f8ba6289750a5f21b597e2e606f9caf3904bfa15
Imtiaz Rahman <63388102+vinzmoke-zoro@users.noreply.github.com>
2024-05-10 19:07:44 -0400

    Delete docs directory

--- docs/search/files_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['create3_5fsimple_5fzigzag_2epy_0',['create3_simple_zigzag.py',['../create3__simple__zigzag_8py.html',1,'']]]
-];
--- docs/tutorial_dup.js
+++
@@ -1,5 +0,0 @@
-var tutorial_dup =
-[
-    [ "__init__.py", "____init_____8py.html", null ],
-    [ "create3_simple_zigzag.py", "create3__simple__zigzag_8py.html", "create3__simple__zigzag_8py" ]
-];
